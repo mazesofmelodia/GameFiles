28a2e5084f931b89ed032544bb9236e874af87dd
Gearóid <gearoid.reilly.gti@gmail.com>
2019-10-31 21:05:42 +0000

    Added Player attacking, they can now damage enemies.

---
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set health to maxHealth
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount){
+        //Player takes damage based on damage amount
+        health -= damageAmount;
+
+        //Check if the player has lost all of their health
+        if(health <= 0){
+            //Player is dead
+            Die();
+        }
+    }
+
+    //Enemy has lost all of their health
+    private void Die(){
+        //Destroy the gameobject
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private Animator anim;
+    [SerializeField] private int damage;        //Player damage
+    [SerializeField] private float attackSpeed; //Attack speed
+    [SerializeField] private float range;       //Attack range
+
+    [Space]
+    [SerializeField] private Transform attackPoint; //For melee attacks
+
+    private Animator anim;                      //Reference to animator component
+    private float timeBetweenAttack;            //Time passed since attack
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +24,44 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Check if the player has clicked the attack button
-        if(Input.GetButtonDown("Attack")){
-            //Play the attacking animation
-            anim.SetTrigger("Attacking");
+        if(timeBetweenAttack <= 0){
+            //Check if the player has clicked the attack button
+            if(Input.GetButtonDown("Attack")){
+                //Play the attacking animation
+                anim.SetTrigger("Attacking");
+
+                //Set time between attack to be the attack speed value
+                timeBetweenAttack = attackSpeed;
+            }
+
+        }else{
+            //Decrease time between attack over time
+            timeBetweenAttack -= Time.deltaTime;
         }
+    }
+
+    public void Attack(){
+        Debug.Log("Attacking");
+
+        //Emit a sphere to hurt objects in the scene
+        Collider[] attackedObjects = Physics.OverlapSphere(attackPoint.position, range);
+
+        //Loop through all hit objects
+        for (int i = 0; i < attackedObjects.Length; i++)
+        {
+            //Check if the hit object has an Enemy stats component
+            EnemyStats hitEnemy = attackedObjects[i].GetComponent<EnemyStats>();
+
+            if(hitEnemy != null){
+                //Damage the enemy
+                hitEnemy.TakeDamage(damage);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(attackPoint.position, range);
     }
 }
