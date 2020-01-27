f6c408e6fb95097ff8f8bc26301229872b88658a
Patrick McHugh <patrickmchughmedia@gmail.com>
2020-01-27 20:29:54 +0000

    Graphics Setting added

--- Assets/Scripts/Audio/SettingsMenu.cs
+++ Assets/Scripts/Audio/SettingsMenu.cs
@@ -11,4 +11,10 @@ public class SettingsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("Volume", Volume);
     }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
 }
